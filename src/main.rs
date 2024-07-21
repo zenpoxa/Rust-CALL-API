c96c71778e6b736f2f36fbac6bb1efc226b7c001
zenpoxa <zenpoxa@gmail.com>
2024-07-21 17:41:54 +0200

    réussit pour faire des API CALLS

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,12 @@
-use serde::Deserialize;
-use reqwest::Error;
-use reqwest::header::USER_AGENT;
-
-#[derive(Deserialize, Debug)]
-struct User {
-    login: String,
-    id: u32,
-}
-
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",
-                              owner = "rust-lang-nursery",
-                              repo = "rust-cookbook");
-    println!("{}", request_url);
-    let client = reqwest::Client::new();
-    let response = client
-        .get(&request_url)
-        .header(USER_AGENT, "rust web-api-client demo")
-        .send()
-        .await?;
+async fn main() -> Result<(), reqwest::Error> {
+    let url : &str = "http://api.open-notify.org/astros.json";
+    let res: serde_json::Value = reqwest::get(url).await?.json().await?;
 
-    let users: Vec<User> = response.json().await?;
-    println!("{:?}", users);
+    let n = &res["number"];
+    let n = n.as_u64().unwrap();
+
+    println!("{n}");
+
     Ok(())
 }
